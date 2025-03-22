2f8a43e4f482e4b72dbc423fc22f1866c88bf79e
Назар Стецюк <ne1rohhhhh@gmail.com>
2025-03-23 01:06:42 +0200

    mobile-container + burger + pixel perfect

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,26 @@
-  
+const buttonBurger = document.querySelector('.btn-burger');
+const mobileContainer = document.querySelector('.mobile-container');
+const bodyNoScroll = document.querySelector('body');
+
+function checkClass() {
+    mobileContainer.classList.toggle('show');
+    bodyNoScroll.classList.toggle('overflow-hidden');
+    buttonBurger.classList.toggle('active');
+}
+
+buttonBurger.addEventListener('click', checkClass);
+
+/*
+buttonBurger.addEventListener('click', function() {
+    console.log('click');
+})
+*/
+
+/*
+function writeConsole() {
+    console.log('click');
+}
+
+buttonBurger.addEventListener('click', writeConsole);
+*/
+
